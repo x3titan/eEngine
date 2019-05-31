b99e5abb1885895f499667ce34ecb72bb0e9a4de
x3titan <31681151+x3titan@users.noreply.github.com>
2019-06-01 00:45:55 +0800

    simple test for js

---
+++ jsTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tam
+ * Date: 2019/6/1
+ * Time: 0:26
+ */
+require_once(dirname(__FILE__) . '/skipif.inc');
+$JS = <<< EOT
+len = print('Hello' + ' ' + 'World!' + "\\n");
+len;
+EOT;
+$v8 = new V8Js();
+try {
+    var_dump($v8->executeString($JS, 'basic.js'));
+} catch (V8JsScriptException $e) {
+    var_dump($e);
+}
+
+
+?>
